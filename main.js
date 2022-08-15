03fccbc6871dcd4c25591f7aa7c75fb0423a5960
Fuad303 <sadiqovfuad8@gmail.com>
2022-08-15 09:25:58 +0400

    some possibly meaningful message

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-var imgList = document.getElementById('imgList');
-var scrollRight = document.getElementById('scroll-right');
-var scrollLeft = document.getElementById('scroll-left');
-
-scrollRight.addEventListener('click', (event) => {
-  imgList.scrollBy(350, 0);
-});
-
-scrollLeft.addEventListener('click', (event) => {
-  imgList.scrollBy(-350, 0);
-});
-
-
 // Fancy Box
 $('[data-fancybox]').fancybox({
   buttons : [
